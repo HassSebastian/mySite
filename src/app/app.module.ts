c1c2ccb6d06ca8adbb1eaf0e9b03a9797530c798
HassSebastian <sebastian.hass76@gmail.com>
2023-04-26 21:57:23 +0200

    header and headline add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ContentSiteComponent } from './content-site/content-site.component';
+import { HeadlineComponent } from './content-site/headline/headline.component';
+import { AboutMeComponent } from './content-site/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentSiteComponent,
+    HeadlineComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ModalModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
