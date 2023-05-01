5ac770da60911b1fd67abce5ea5f27cdace120c1
HassSebastian <sebastian.hass76@gmail.com>
2023-05-01 18:28:38 +0200

    portfolio animation in progress

--- src/app/footer/footer-message-area/footer-message-area.component.ts
+++ src/app/footer/footer-message-area/footer-message-area.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+
 
 @Component({
   selector: 'app-footer-message-area',
@@ -6,5 +17,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer-message-area.component.scss']
 })
 export class FooterMessageAreaComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 
 }
