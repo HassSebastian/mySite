fe6d6dcccc3e07b4c04c3d8adaef38e688c8d589
HassSebastian <sebastian.hass76@gmail.com>
2023-05-03 20:28:17 +0200

    menu-button add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'out',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('in => out', animate('300ms')),
+      transition('out => in', animate('300ms')),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  showMenu = false;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
 }
