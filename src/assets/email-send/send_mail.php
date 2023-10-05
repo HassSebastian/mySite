83a4b7ab7685c79ee9178ff57e2532dcccb9b624
HassSebastian <sebastian.hass76@gmail.com>
2023-10-05 17:05:41 +0200

    php edit

--- src/assets/email-send/send_mail.php
+++ src/assets/email-send/send_mail.php
@@ -1,122 +1,36 @@
-<!-- <?php
-
-########### CONFIG ###############
-
-$recipient = 'info@sebastian-hass.de'; // Bitte hier deine E-Mail-Adresse angeben
-
-########### CONFIG END ###########
-
-
-if (empty($recipient)) {
-    die("Bitte geben Sie die E-Mail-Adresse in Zeile 5 an.");
-}
-
-header('Content-Type: text/html; charset=utf-8');
-// CORS headers
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
-// Funktion zur korrekten Kodierung von Umlauten in E-Mails
-function encodeEmailText($text) {
-    $text = utf8_encode($text);
-    return $text;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    // Prüfen, ob die erforderlichen Felder vorhanden sind
-    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-
-        // Überprüfen, ob die E-Mail-Adresse gültig ist
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $subject = "Kontakt von " . $name . " - E-Mail: " . $email;
-            $headers = "From: $email";
-
-            // Kodieren Sie den Betreff und die Nachricht
-            $encodedSubject = encodeEmailText($subject);
-            $encodedMessage = encodeEmailText($message);
-
-            if (mail($recipient, $encodedSubject, $encodedMessage, $headers)) {
-                // Erfolgreich versendet, sende Bestätigung
-                echo "gesendet";
-
-                // Bestätigungs-E-Mail an den Absender senden
-                $confirmationSubject = "Vielen Dank";
-                $confirmationMessage = "Vielen Dank. Ich werde mich sehr bald bei Ihnen Melden.";
-
-                // Kodieren Sie den Betreff und die Nachricht der Bestätigungs-E-Mail
-                $encodedConfirmationSubject = encodeEmailText($confirmationSubject);
-                $encodedConfirmationMessage = encodeEmailText($confirmationMessage);
-
-                $confirmationHeaders = "From: $recipient";
-                mail($email, $encodedConfirmationSubject, $encodedConfirmationMessage, $confirmationHeaders);
-            } else {
-                // Fehler beim Versenden
-                echo "fehler";
-            }
-        } else {
-            echo "ungültige_email";
-        }
-    } else {
-        echo "fehlende_felder";
-    }
-} else {
-    header("Allow: POST", true, 405);
-    exit;
-}
-?> -->
-
-
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
-########### CONFIG ###############
+  // E-Mail an den Empfänger senden
+  $to = 'seb-ha@gmx.de';
+  $subject = 'Neue Message';
+  $headers = "From: webmaster@example.com\r\n";
+  $headers .= "Reply-To: webmaster@example.com\r\n";
+  $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-$recipient = 'info@sebastian-hass.de'; // Bitte hier deine E-Mail-Adresse angeben
+  $emailMessage = "Neue Message:\n\n";
+  $emailMessage .= "Name: $name\n";
+  $emailMessage .= "Email: $email\n";
+  $emailMessage .= "Nachricht: $message\n";
 
-########### CONFIG END ###########
+  mail($to, $subject, $emailMessage, $headers);
 
+  // Bestätigungsmail an den Absender senden
+  $confirmationSubject = 'Ihre Nachricht wurde erfolgreich gesendet';
+  $confirmationMessage = 'Vielen Dank für Ihre Nachricht.\n';
+  $confirmationMessage = 'Ich haben Ihre Anfrage erhalten und werde mich in Kürze bei Ihnen melden.';
+  $confirmationHeaders .= "From: $to\r\n";
+  $confirmationHeaders .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-if (empty($recipient)) {
-    die("Bitte geben Sie die E-Mail-Adresse in Zeile 5 an.");
-}
-
-// CORS headers
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    // Prüfen, ob die erforderlichen Felder vorhanden sind
-    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-
-        // Überprüfen, ob die E-Mail-Adresse gültig ist
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $subject = "Kontakt von " . $name . " - E-Mail: " . $email;
-            $headers = "From: " . $email . "\r\n";
-            $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-            $message = "Vielen Dank für Ihre E-Mail. Ich werde so bald wie möglich antworten.\r\n\r\n" . $message;
-
-            if (mail($recipient, $subject, $message, $headers)) {
-                // E-Mail erfolgreich gesendet
-                echo "success";
-            } else {
-                echo "error";
-            }
-        } else {
-            echo "error";
-        }
-    } else {
-        echo "error";
-    }
+  // JSON-Antwort an den TypeScript-Teil senden
+  $response = ['status' => 'success', 'message' => 'Message erfolgreich gesendet'];
+  mail($email, $confirmationSubject, $confirmationMessage, $confirmationHeaders);
+  echo json_encode($response);
 } else {
-    header("Allow: POST", true, 405);
-    exit;
+  header('HTTP/1.1 405 Method Not Allowed');
+  echo 'Method Not Allowed';
 }
 ?>
